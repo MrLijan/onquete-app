4c355d156781b5e1d1070fa69a8fa3237171c579
MrLijan <liramj21@gmail.com>
2022-07-16 18:44:29 +0300

    feat/Route for creating new survey

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "create",
+    loadChildren: () => import("./components/_pages/new-survey/new-survey.module").then(m => m.NewSurveyModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
