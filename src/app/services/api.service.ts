6e25bb6ba48df205eb1287ba0b95dbb179543de9
MrLijan <liramj21@gmail.com>
2022-08-07 22:22:06 +0300

    Added api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,32 +1,27 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  basePath: string = '';
 
   constructor(
     private http: HttpClient,
   ) { }
 
-  public async get(path: string) {
-    return this.http.get(path);
+  public async get(path: string): Promise<Observable<any>> {
+    return this.send('GET', path);
   }
 
-  public async post(path: string, payload: any) {
-    return this.http.post(path, payload);
+  public async post(path: string, payload: Object): Promise<Observable<any>> {
+    return this.send('POST', path, payload);
   }
 
-  public async patch(path: string, payload: any) {
-    return this.http.patch(path, payload);
-  }
-
-  public async delete(path: string, payload: any) {
-    return this.http.delete(path, payload);
-  }
-
-  public async put(path: string, payload: any) {
-    return this.http.put(path, payload);
+  private async send(method: string, path: string, body: any = ''): Promise<Observable<any>> {
+    const url = this.basePath + path;
+    return this.http.request(method, url, body ?? undefined);
   }
 }
